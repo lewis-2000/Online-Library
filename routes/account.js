e8e91c92031dc22f5ce4464744ae4ef0ca99c281
Lewis Ng'ang'a <59686000+lewis-2000@users.noreply.github.com>
2023-10-26 12:59:19 +0300

    Added account page
    Accounts page

--- routes/account.js
+++ routes/account.js
@@ -4,8 +4,29 @@ const router = express.Router();
 router.get('/', function (req, res) {
     res.render('account');
 
-}).post(function (req, res){
-    res.render('account');
-});
+}).post('/account', async function (req, res) {
+    try {
+      const bookData = req.body;
+  
+      await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection('mycollection'); // Change collection name as needed
+  
+      // Insert the book data into your collection
+      const result = await collection.insertOne(bookData);
+  
+      if (result.insertedCount === 1) {
+        res.json({ success: true });
+      } else {
+        res.json({ success: false });
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      res.json({ success: false });
+    } finally {
+      await client.close();
+      console.log("Database connection closed");
+    }
+  });
 
 module.exports = router;
--- routes/search.js
+++ routes/search.js
@@ -49,29 +49,6 @@ router.get('/',async function (req, res) {
     }
   
 
-}).post('/add-to-collection', async function (req, res) {
-    try {
-      const bookData = req.body; // Assuming you send book data in the request body
-  
-      await client.connect();
-      const db = client.db(dbName);
-      const collection = db.collection('mycollection'); // Change collection name as needed
-  
-      // Insert the book data into your collection
-      const result = await collection.insertOne(bookData);
-  
-      if (result.insertedCount === 1) {
-        res.json({ success: true });
-      } else {
-        res.json({ success: false });
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      res.json({ success: false });
-    } finally {
-      await client.close();
-      console.log("Database connection closed");
-    }
-  });
+});
 
 module.exports = router;
